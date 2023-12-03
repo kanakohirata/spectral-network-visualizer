39a55cf6bc1c2fddea8210d7a08f84f27776a25e
kanakohirata <khirata00000@gmail.com>
2023-12-03 20:21:44 +0900

    Add multilayer_3d_network.make_edges_and_nodes_inter_layer()

--- app/snv_pub/visualizer/processing/multilayer_3d_ms_network/multilayer_3d_network.py
+++ app/snv_pub/visualizer/processing/multilayer_3d_ms_network/multilayer_3d_network.py
@@ -401,6 +401,83 @@ def make_edges_and_nodes_inner_layer(list_attribute_for_layer: list,
     return list_dic_edges_nodes_graph_by_layer
 
 
+def make_edges_and_nodes_inter_layer(list_of_edge_for_networkx,
+                                     dic_cluster_total_input_idx_MOD_vs_node_info,
+                                     fo_log_key):
+    """
+
+    Parameters
+    ----------
+    list_of_edge_for_networkx : list
+    dic_cluster_total_input_idx_MOD_vs_node_info : list
+    fo_log_key : file object
+
+    Returns
+    -------
+    list_of_edge_for_networkx_to_show_inter_sample_ref_layer : list
+    list_of_edge_for_networkx_to_show_inter_sample_layer : list
+    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer : dict
+    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer : dict
+    list_total_input_idx_MOD_inter_sample_ref_layer : list
+    list_total_input_idx_MOD_inter_sample_layer : list
+    """
+    # [EDGES]
+    # create list_od_edge for NetworkX for particular layer. -----------------------------------
+    list_of_edge_for_networkx_to_show_inter_sample_ref_layer = []
+    list_of_edge_for_networkx_to_show_inter_sample_layer = []
+    # this list keeps total_input_idx_MOD for "inter sample ref layer"
+    list_total_input_idx_MOD_inter_sample_ref_layer = []
+    list_total_input_idx_MOD_inter_sample_layer = []
+
+    logger.debug(f" len of list_of_edge_for_networkx_to_show {len(list_of_edge_for_networkx)}")
+    for edge_for_networkx in list_of_edge_for_networkx:
+        # we are only looking for inner layer edge
+
+        if edge_for_networkx[2]["edge_type"] == "inter_sample_ref_layer":
+            # if the edge is ""inter sample ref edge",
+            list_of_edge_for_networkx_to_show_inter_sample_ref_layer.append(edge_for_networkx)
+            list_total_input_idx_MOD_inter_sample_ref_layer.append(edge_for_networkx[0])
+            list_total_input_idx_MOD_inter_sample_ref_layer.append(edge_for_networkx[1])
+
+        if edge_for_networkx[2]["edge_type"] == "inter_sample_layer":
+            list_of_edge_for_networkx_to_show_inter_sample_layer.append(edge_for_networkx)
+            list_total_input_idx_MOD_inter_sample_layer.append(edge_for_networkx[0])
+            list_total_input_idx_MOD_inter_sample_layer.append(edge_for_networkx[1])
+
+    fo_log_key.write("\n list_of_edge_for_networkx_to_show_inter_sample_ref_layer : "
+                     f"{len(list_of_edge_for_networkx_to_show_inter_sample_ref_layer)}")
+    fo_log_key.write("\n list_of_edge_for_networkx_to_show_inter_sample_layer : "
+                     f"{len(list_of_edge_for_networkx_to_show_inter_sample_layer)}")
+    fo_log_key.flash()
+    logger.info(f'Length of list_of_edge_for_networkx_to_show_inter_sample_ref_layer: '
+                f'{len(list_of_edge_for_networkx_to_show_inter_sample_ref_layer)}')
+    logger.info(f'Length of ist_of_edge_for_networkx_to_show_inter_sample_layer: '
+                f'{len(list_of_edge_for_networkx_to_show_inter_sample_layer)}')
+
+    # make it non redundant.
+    list_total_input_idx_MOD_inter_sample_ref_layer = list(set(list_total_input_idx_MOD_inter_sample_ref_layer))
+    list_total_input_idx_MOD_inter_sample_layer = list(set(list_total_input_idx_MOD_inter_sample_layer))
+
+    # [NODES]
+    #  create dic  _cluster_total_input_idx_MOD_vs_node_info for each layer ------------------------
+    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer = {}
+    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer = {}
+
+    for cluster_total_input_idx_MOD, node_info in dic_cluster_total_input_idx_MOD_vs_node_info.items():
+        if cluster_total_input_idx_MOD in list_total_input_idx_MOD_inter_sample_ref_layer:
+            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer[cluster_total_input_idx_MOD] = node_info
+
+        if cluster_total_input_idx_MOD in list_total_input_idx_MOD_inter_sample_layer:
+            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer[cluster_total_input_idx_MOD] = node_info
+
+    return (list_of_edge_for_networkx_to_show_inter_sample_ref_layer,
+            list_of_edge_for_networkx_to_show_inter_sample_layer,
+            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer,
+            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer,
+            list_total_input_idx_MOD_inter_sample_ref_layer,
+            list_total_input_idx_MOD_inter_sample_layer)
+
+
 def remove_node_edge_with_no_layer_attribute(config_o, dic_cluster_total_input_idx_MOD_vs_node_info,
                                              list_of_edge_for_networkx):
     #####################
@@ -2058,55 +2135,16 @@ def process_3d_network_data(dic_source_data, dic_config):
     fo_log_key.write("\n\n [L1 ]making dataset for INTER  SAMPLE VS REF  LAYER  and INTER SAMPLE dataset")
     logger.debug("[L1] making dataset for inter sample vs ref layer  and inter sample dataset")
     logger.info("[L1] making dataset for inter sample vs ref layer  and inter sample dataset")
-    list_dic_edges_nodes_inter_layer = []
 
-    # [EDGES]
-    # create list_od_edge for NetworkX for particular layer. -----------------------------------
-    list_of_edge_for_networkx_to_show_inter_sample_ref_layer = []
-    list_of_edge_for_networkx_to_show_inter_sample_layer = []
-    # this list keeps total_input_idx_MOD for "inter sample ref layer"
-    list_total_input_idx_MOD_inter_sample_ref_layer = []
-    list_total_input_idx_MOD_inter_sample_layer = []
-
-    logger.debug(f" len of list_of_edge_for_networkx_to_show {len(list_of_edge_for_networkx_to_show)}")
-    for edge_for_networkx in list_of_edge_for_networkx_to_show:
-        # we are only looking for inner layer edge
-
-        if edge_for_networkx[2]["edge_type"] == "inter_sample_ref_layer":
-            # if the edge is ""inter sample ref edge",
-            list_of_edge_for_networkx_to_show_inter_sample_ref_layer.append(edge_for_networkx)
-            list_total_input_idx_MOD_inter_sample_ref_layer.append(edge_for_networkx[0])
-            list_total_input_idx_MOD_inter_sample_ref_layer.append(edge_for_networkx[1])
-
-        if edge_for_networkx[2]["edge_type"] == "inter_sample_layer":
-            list_of_edge_for_networkx_to_show_inter_sample_layer.append(edge_for_networkx)
-            list_total_input_idx_MOD_inter_sample_layer.append(edge_for_networkx[0])
-            list_total_input_idx_MOD_inter_sample_layer.append(edge_for_networkx[1])
-
-    fo_log_key.write("\n list_of_edge_for_networkx_to_show_inter_sample_ref_layer : " + str(
-        len(list_of_edge_for_networkx_to_show_inter_sample_ref_layer)))
-    fo_log_key.write("\n list_of_edge_for_networkx_to_show_inter_sample_layer : " + str(
-        len(list_of_edge_for_networkx_to_show_inter_sample_layer)))
-    logger.info(f'Length of list_of_edge_for_networkx_to_show_inter_sample_ref_layer: '
-                f'{len(list_of_edge_for_networkx_to_show_inter_sample_ref_layer)}')
-    logger.info(f'Length of ist_of_edge_for_networkx_to_show_inter_sample_layer: '
-                f'{len(list_of_edge_for_networkx_to_show_inter_sample_layer)}')
-
-    # make it non redundant.
-    list_total_input_idx_MOD_inter_sample_ref_layer = list(set(list_total_input_idx_MOD_inter_sample_ref_layer))
-    list_total_input_idx_MOD_inter_sample_layer = list(set(list_total_input_idx_MOD_inter_sample_layer))
-
-    # [NODES]
-    #  create dic  _cluster_total_input_idx_MOD_vs_node_info for each layer ------------------------
-    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer = {}
-    for cluster_total_input_idx_MOD, node_info in dic_cluster_total_input_idx_MOD_vs_node_info.items():
-        if cluster_total_input_idx_MOD in list_total_input_idx_MOD_inter_sample_ref_layer:
-            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer[cluster_total_input_idx_MOD] = node_info
-
-    dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer = {}
-    for cluster_total_input_idx_MOD, node_info in dic_cluster_total_input_idx_MOD_vs_node_info.items():
-        if cluster_total_input_idx_MOD in list_total_input_idx_MOD_inter_sample_layer:
-            dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer[cluster_total_input_idx_MOD] = node_info
+    (list_of_edge_for_networkx_to_show_inter_sample_ref_layer,
+     list_of_edge_for_networkx_to_show_inter_sample_layer,
+     dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_ref_layer,
+     dic_cluster_total_input_idx_MOD_vs_node_info_inter_sample_layer,
+     list_total_input_idx_MOD_inter_sample_ref_layer,
+     list_total_input_idx_MOD_inter_sample_layer) =\
+        make_edges_and_nodes_inter_layer(list_of_edge_for_networkx_to_show,
+                                         dic_cluster_total_input_idx_MOD_vs_node_info,
+                                         fo_log_key)
 
     #############################################################
     ########################################################
