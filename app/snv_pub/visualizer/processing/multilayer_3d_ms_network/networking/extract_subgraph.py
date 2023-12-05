92ebb34e6511653bbe7d12199b069e48704a0103
kanakohirata <khirata00000@gmail.com>
2023-12-05 13:32:11 +0900

    Add networking.extract_subgraph.extract_ref_subgraph_in_external_compounds()

--- app/snv_pub/visualizer/processing/multilayer_3d_ms_network/networking/extract_subgraph.py
+++ app/snv_pub/visualizer/processing/multilayer_3d_ms_network/networking/extract_subgraph.py
@@ -147,7 +147,7 @@ def extract_ref_subgraph_connected_to_sample(list_dic_edges_nodes_graph_by_layer
         # 'foundset' is a set of node names that are connected to nodes in 'list_total_input_idx_mod_for_base'
         # within a depth equal to or less than 'subgraph_depth'.
         foundset = {key for source in list_total_input_idx_mod_for_base for key in
-                    list(nx.single_source_shortest_path(nx_graph, source,cutoff=subgraph_depth).keys())}
+                    list(nx.single_source_shortest_path(nx_graph, source, cutoff=subgraph_depth).keys())}
         nx_graph_sub = nx_graph.subgraph(foundset)
 
         # create list of edges again.  nx.generate_edgelist  not really working ???
@@ -164,6 +164,75 @@ def extract_ref_subgraph_connected_to_sample(list_dic_edges_nodes_graph_by_layer
         dic_cluster_total_input_idx_MOD_vs_node_info_UPDATE = {}
         for cluster_total_input_idx_MOD, node_info in \
                 dic_edges_nodes_graph_by_layer['dic_cluster_total_input_idx_MOD_vs_node_info'].items():
+            if cluster_total_input_idx_MOD in l_total_input_idx_mod_IN_subgraph:
+                dic_cluster_total_input_idx_MOD_vs_node_info_UPDATE[cluster_total_input_idx_MOD] = node_info
+
+        dic_edges_nodes_graph_by_layer_new = {
+            'attribute_for_layer': dic_edges_nodes_graph_by_layer['attribute_for_layer'],
+            'nx_graph': nx_graph_sub,
+            'list_of_edge_for_networkx': list_of_edge_for_networkx_sub,
+            'dic_cluster_total_input_idx_MOD_vs_node_info': dic_cluster_total_input_idx_MOD_vs_node_info_UPDATE}
+
+        list_dic_edges_nodes_graph_by_layer_new.append(dic_edges_nodes_graph_by_layer_new)
+
+    return list_dic_edges_nodes_graph_by_layer_new
+
+
+def extract_ref_subgraph_in_external_compounds(list_dic_edges_nodes_graph_by_layer: list,
+                                               subgraph_depth: int):
+    """
+
+    Parameters
+    ----------
+    list_dic_edges_nodes_graph_by_layer : list
+    subgraph_depth : int
+
+    Returns
+    -------
+    list_dic_edges_nodes_graph_by_layer_new : list
+        Updated list_dic_edges_nodes_graph_by_layer
+    """
+    # Update list_dic_edges_nodes_graph_by_layer
+    list_dic_edges_nodes_graph_by_layer_new = []
+    # look for ref layer---------------------------------
+    for dic_edges_nodes_graph_by_layer in list_dic_edges_nodes_graph_by_layer:
+        # choosing ref layer (layer tag is not sample.)
+        if dic_edges_nodes_graph_by_layer['attribute_for_layer'].startswith('sample'):
+            list_dic_edges_nodes_graph_by_layer_new.append(dic_edges_nodes_graph_by_layer)
+            continue
+
+        # selecting node based on external compound info.
+        list_total_input_idx_mod_for_subgraph = []
+        for cluster_total_input_idx_MOD, node_info in\
+                dic_edges_nodes_graph_by_layer['dic_cluster_total_input_idx_MOD_vs_node_info'].items():
+            if len(node_info.spec_cluster.list_compound_categories) > 0:
+                list_total_input_idx_mod_for_subgraph.append(cluster_total_input_idx_MOD)
+        nx_graph = dic_edges_nodes_graph_by_layer['nx_graph']
+
+        # 'foundset' is a set of node names that are connected to nodes in 'list_total_input_idx_mod_for_subgraph'
+        # within a depth equal to or less than 'subgraph_depth'.
+        foundset = {key for source in list_total_input_idx_mod_for_subgraph for key in
+                    list(nx.single_source_shortest_path(nx_graph, source, cutoff=subgraph_depth).keys())}
+
+        # update and replace nx graph and other info
+        nx_graph_sub = nx_graph.subgraph(foundset)
+
+        # create list of edges again.  nx.generate_edgelist  not really working ???
+        list_of_edge_for_networkx_sub = []
+        for e in nx_graph_sub.edges.data():
+            list_of_edge_for_networkx_sub.append([e[0], e[1], e[2]])
+
+        # updating "dic_cluster_total_input_idx_MOD_vs_node_info" -----------------------------
+
+        # this has total input idx mod PRESENT in subgraph
+        l_total_input_idx_mod_IN_subgraph = []
+        for node in nx_graph_sub.nodes(data=True):
+            l_total_input_idx_mod_IN_subgraph.append(node[0])
+
+        dic_cluster_total_input_idx_MOD_vs_node_info_UPDATE = {}
+        for cluster_total_input_idx_MOD, node_info in\
+                dic_edges_nodes_graph_by_layer['dic_cluster_total_input_idx_MOD_vs_node_info'].items():
+
             if cluster_total_input_idx_MOD in l_total_input_idx_mod_IN_subgraph:
                 dic_cluster_total_input_idx_MOD_vs_node_info_UPDATE[cluster_total_input_idx_MOD] = node_info
 
